6c33948f78e03ed807b7ce7865551439e928aacf
Ahmed Aboelnaga <iaboelnaga97@gmail.com>
2022-05-30 00:29:52 +0200

    Initial Commit

---
+++ CollectionScrollView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionScrollView
+//
+//  Created by Aboelnaga on 30/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
